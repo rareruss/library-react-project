19a137c2b33789d1473f559ac437cee4eeb38767
Russell <ruselllouviere5315@gmail.com>
2024-08-31 15:16:04 -0500

    first commit library react project

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Landing from "../Components/Landing";
+import Highlights from "../Components/Highlights";
+import Featured from "../Components/Featured";
+import Discounted from "../Components/Discounted";
+import Explore from "../Components/Explore";
+
+export default function Home() {
+  return (
+    <>
+      <Landing />
+      <Highlights />
+      <Featured />
+      <Discounted />
+      <Explore />
+    </>
+  );
+}
